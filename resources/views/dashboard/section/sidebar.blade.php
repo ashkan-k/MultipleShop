1379ffa866ac284c83e1ba437f59798c081644a9
ashkan karimi <karimiashkan8186@gmail.com>
2023-06-28 21:44:07 +0330

    add Order Module and Crud

--- resources/views/dashboard/section/sidebar.blade.php
+++ resources/views/dashboard/section/sidebar.blade.php
@@ -308,6 +308,35 @@
             </div>
 
             <div data-kt-menu-trigger="click"
+                 class="menu-item @if(str_starts_with(\Illuminate\Support\Facades\Route::current()->getName(), 'orders.')) show @endif menu-accordion">
+                <span class="menu-link">
+											<span class="menu-icon">
+												<i class="ki-duotone ki-shop fs-2">
+													<span class="path1"></span>
+													<span class="path2"></span>
+													<span class="path3"></span>
+													<span class="path4"></span>
+												</i>
+											</span>
+											<span class="menu-title">سفارشات</span>
+											<span class="menu-arrow"></span>
+										</span>
+                <div class="menu-sub menu-sub-accordion">
+
+                    <div class="menu-item">
+                        <a class="menu-link @if(\Illuminate\Support\Facades\Route::current()->getName() == 'orders.index') active @endif"
+                           href="{{ route('orders.index') }}">
+													<span class="menu-bullet">
+														<span class="bullet bullet-dot"></span>
+													</span>
+                            <span class="menu-title">لیست سفارشات</span>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div data-kt-menu-trigger="click"
                  class="menu-item @if(str_starts_with(\Illuminate\Support\Facades\Route::current()->getName(), 'coupons.')) show @endif menu-accordion">
                 <span class="menu-link">
 											<span class="menu-icon">
