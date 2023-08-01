b7e84cb4fce720c0c7c4b95b6febeece9c35412c
ashkan_k <karimiashkan8181@gmail.com>
2023-08-01 15:36:46 +0330

    add Buy Feature init

---
+++ database/migrations/2023_08_01_114838_add_feature_value_and_feature_id_to_orders.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_products', function (Blueprint $table) {
+            $table->string('feature_value')->nullable();
+            $table->foreignId('feature_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('', function (Blueprint $table) {
+
+        });
+    }
+};
