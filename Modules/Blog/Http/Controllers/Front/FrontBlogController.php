fe1b0b1d858106a3c620fcb69e195805c1754f4a
ashkan_k <karimiashkan8181@gmail.com>
2023-08-06 21:18:17 +0330

    ****

--- Modules/Blog/Http/Controllers/Front/FrontBlogController.php
+++ Modules/Blog/Http/Controllers/Front/FrontBlogController.php
@@ -30,7 +30,6 @@ class FrontBlogController extends Controller
     {
         $blog = Blog::FindBySlug($lang, $slug);
         $blog_schema = SchemaHelper::GetArticleSchema($blog);
-        return view('blog::aaa');
         return view('blog::front.blog-detail', compact('blog', 'blog_schema'));
     }
 }
