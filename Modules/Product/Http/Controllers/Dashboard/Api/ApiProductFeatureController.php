e0c57a9483443d9eb5770fa00be268147401304c
ashkan_k <karimiashkan8181@gmail.com>
2023-08-07 13:36:23 +0330

    ****

--- Modules/Product/Http/Controllers/Dashboard/Api/ApiProductFeatureController.php
+++ Modules/Product/Http/Controllers/Dashboard/Api/ApiProductFeatureController.php
@@ -6,6 +6,7 @@ use App\Http\Traits\Responses;
 use App\Http\Traits\Uploader;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Validation\ValidationException;
 use Modules\Product\Entities\Gallery;
 use Modules\Product\Entities\Product;
 use Modules\Product\Entities\ProductFeature;
@@ -20,6 +21,15 @@ class ApiProductFeatureController extends Controller
 
     private $relations = ['product', 'feature'];
 
+    private function CheckDuplicateFeature($product_id, $feature_id, $object_id=null)
+    {
+        if (ProductFeature::where('product_id', $product_id)->where('feature_id', $feature_id)->whereNot('id', $object_id)->exists()){
+            throw ValidationException::withMessages(['feature_id' => 'این ویژگی قبلا به محصول الحاق شده است!'])->status(400);
+        }
+    }
+
+    //
+
     public function index()
     {
         $objects = ProductFeature::where('product_id', \request('product_id'))
@@ -29,12 +39,14 @@ class ApiProductFeatureController extends Controller
 
     public function store(ProductFeatureRequest $request)
     {
+        $this->CheckDuplicateFeature($request->product_id, $request->feature_id);
         ProductFeature::create($request->validated());
         return $this->SuccessResponse('آیتم مورد نظر با موفقیت ثبت شد.');
     }
 
     public function update(ProductFeatureRequest $request, ProductFeature $products_feature)
     {
+        $this->CheckDuplicateFeature($request->product_id, $request->feature_id, $products_feature->id);
         $products_feature->update($request->validated());
         return $this->SuccessResponse('آیتم مورد نظر با موفقیت ویرایش شد.');
     }
